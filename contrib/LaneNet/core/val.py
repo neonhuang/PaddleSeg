dade82058f2bfb02f94e9e0cab8325c973665f10
huangshenghui <huangshenghui@baidu.com>
2021-11-07 10:11:20 +0800

    update code

--- contrib/LaneNet/core/val.py
+++ contrib/LaneNet/core/val.py
@@ -88,7 +88,7 @@ def evaluate(model, eval_dataset, num_workers=0, print_detail=True):
                 ori_shape=ori_shape,
                 transforms=eval_dataset.transforms.transforms)
 
-            postprocessor.evaluate(eval_dataset, pred, data)
+            postprocessor.evaluate(pred, data)
         best_acc, acc, fp, fn, eval_result = postprocessor.summarize()
         logger.info(eval_result)
     return best_acc, best_acc, best_acc, best_acc, best_acc, acc, fn, fp
--- contrib/LaneNet/utils/tusimple.py
+++ contrib/LaneNet/utils/tusimple.py
@@ -2,6 +2,7 @@ import paddle.nn.functional as F
 import json
 import os
 import cv2
+import numpy as np
 
 from .lane import LaneEval
 
@@ -36,58 +37,60 @@ class Tusimple:
         # if cfg.view:
         #     self.view_dir = os.path.join(self.cfg.work_dir, 'vis')
 
-    def evaluate_pred(self, dataset, seg_pred, exist_pred, batch):
+    def evaluate_pred(self, seg_pred, exist_pred, batch):
         img_name = batch['meta']['img_name']
         img_path = batch['meta']['full_img_path']
         for b in range(len(seg_pred)):
             seg = seg_pred[b]
             exist = [1 if exist_pred[b, i] >
                           0.5 else 0 for i in range(7 - 1)]
-            lane_coords = dataset.probmap2lane(seg, exist, thresh=self.thresh)
+            lane_coords = self.probmap2lane(seg, exist, thresh=self.thresh)
             for i in range(len(lane_coords)):
                 lane_coords[i] = sorted(
                     lane_coords[i], key=lambda pair: pair[1])
-
-            path_tree = split_path(img_name[b])
-            save_dir, save_name = path_tree[-3:-1], path_tree[-1]
-            save_dir = os.path.join(self.out_path, *save_dir)
-            save_name = save_name[:-3] + "lines.txt"
-            save_name = os.path.join(save_dir, save_name)
-            if not os.path.exists(save_dir):
-                os.makedirs(save_dir, exist_ok=True)
-
-            with open(save_name, "w") as f:
-                for l in lane_coords:
-                    for (x, y) in l:
-                        print("{} {}".format(x, y), end=" ", file=f)
-                    print(file=f)
-
-            json_dict = {}
-            json_dict['lanes'] = []
-            json_dict['h_sample'] = []
-            json_dict['raw_file'] = os.path.join(*path_tree[-4:])
-            json_dict['run_time'] = 0
-            for l in lane_coords:
-                if len(l) == 0:
-                    continue
-                json_dict['lanes'].append([])
-                for (x, y) in l:
-                    json_dict['lanes'][-1].append(int(x))
-            for (x, y) in lane_coords[0]:
-                json_dict['h_sample'].append(y)
-            self.dump_to_json.append(json.dumps(json_dict))
+                self.generateJson(img_name[b], lane_coords)
             # if self.cfg.view:
             #     img = cv2.imread(img_path[b])
             #     new_img_name = img_name[b].replace('/', '_')
             #     save_dir = os.path.join(self.view_dir, new_img_name)
             #     dataset.view(img, lane_coords, save_dir)
 
-    def evaluate(self, dataset, output, batch):
-        seg_pred, exist_pred = output[0], output[1] # output['seg'], output['exist']
+    def generateJson(self, img_path, lane_coords):
+        path_tree = split_path(img_path)
+        save_dir, save_name = path_tree[-3:-1], path_tree[-1]
+        save_dir = os.path.join(self.out_path, *save_dir)
+        save_name = save_name[:-3] + "lines.txt"
+        save_name = os.path.join(save_dir, save_name)
+        if not os.path.exists(save_dir):
+            os.makedirs(save_dir, exist_ok=True)
+
+        with open(save_name, "w") as f:
+            for l in lane_coords:
+                for (x, y) in l:
+                    print("{} {}".format(x, y), end=" ", file=f)
+                print(file=f)
+
+        json_dict = {}
+        json_dict['lanes'] = []
+        json_dict['h_sample'] = []
+        json_dict['raw_file'] = os.path.join(*path_tree[-4:])
+        json_dict['run_time'] = 0
+        for l in lane_coords:
+            if len(l) == 0:
+                continue
+            json_dict['lanes'].append([])
+            for (x, y) in l:
+                json_dict['lanes'][-1].append(int(x))
+        for (x, y) in lane_coords[0]:
+            json_dict['h_sample'].append(y)
+        self.dump_to_json.append(json.dumps(json_dict))
+
+    def evaluate(self, output, batch):
+        seg_pred, exist_pred = output[0], output[1]
         seg_pred = F.softmax(seg_pred, axis=1)
         seg_pred = seg_pred.detach().cpu().numpy()
         exist_pred = exist_pred.detach().cpu().numpy()
-        self.evaluate_pred(dataset, seg_pred, exist_pred, batch)
+        self.evaluate_pred(seg_pred, exist_pred, batch)
 
     def summarize(self):
         best_acc = 0
@@ -99,7 +102,116 @@ class Tusimple:
         eval_result, acc, fp, fn = LaneEval.bench_one_submit(output_file,
                         "/home/work/resa/data/tusimple/test_label.json")
 
-        # self.logger.info(eval_result)
         self.dump_to_json = []
         best_acc = max(acc, best_acc)
         return best_acc, acc, fp, fn, eval_result
+
+    def fix_gap(self, coordinate):
+        if any(x > 0 for x in coordinate):
+            start = [i for i, x in enumerate(coordinate) if x > 0][0]
+            end = [i for i, x in reversed(list(enumerate(coordinate))) if x > 0][0]
+            lane = coordinate[start:end + 1]
+            if any(x < 0 for x in lane):
+                gap_start = [i for i, x in enumerate(
+                    lane[:-1]) if x > 0 and lane[i + 1] < 0]
+                gap_end = [i + 1 for i,
+                                     x in enumerate(lane[:-1]) if x < 0 and lane[i + 1] > 0]
+                gap_id = [i for i, x in enumerate(lane) if x < 0]
+                if len(gap_start) == 0 or len(gap_end) == 0:
+                    return coordinate
+                for id in gap_id:
+                    for i in range(len(gap_start)):
+                        if i >= len(gap_end):
+                            return coordinate
+                        if id > gap_start[i] and id < gap_end[i]:
+                            gap_width = float(gap_end[i] - gap_start[i])
+                            lane[id] = int((id - gap_start[i]) / gap_width * lane[gap_end[i]] + (
+                                    gap_end[i] - id) / gap_width * lane[gap_start[i]])
+                if not all(x > 0 for x in lane):
+                    print("Gaps still exist!")
+                coordinate[start:end + 1] = lane
+        return coordinate
+
+    def is_short(self, lane):
+        start = [i for i, x in enumerate(lane) if x > 0]
+        if not start:
+            return 1
+        else:
+            return 0
+
+    def get_lane(self, prob_map, y_px_gap, pts, thresh, resize_shape=None):
+        """
+        Arguments:
+        ----------
+        prob_map: prob map for single lane, np array size (h, w)
+        resize_shape:  reshape size target, (H, W)
+
+        Return:
+        ----------
+        coords: x coords bottom up every y_px_gap px, 0 for non-exist, in resized shape
+        """
+        if resize_shape is None:
+            resize_shape = prob_map.shape
+        h, w = prob_map.shape
+        H, W = resize_shape
+        H -= 160 # self.cfg.cut_height
+
+        coords = np.zeros(pts)
+        coords[:] = -1.0
+        for i in range(pts):
+            y = int((H - 10 - i * y_px_gap) * h / H)
+            if y < 0:
+                break
+            line = prob_map[y, :]
+            id = np.argmax(line)
+            if line[id] > thresh:
+                coords[i] = int(id / w * W)
+        if (coords > 0).sum() < 2:
+            coords = np.zeros(pts)
+        self.fix_gap(coords)
+        # print(coords.shape)
+
+        return coords
+
+    def probmap2lane(self, seg_pred, exist, resize_shape=(720, 1280), smooth=True, y_px_gap=10, pts=56, thresh=0.6):
+        """
+        Arguments:
+        ----------
+        seg_pred:      np.array size (5, h, w)
+        resize_shape:  reshape size target, (H, W)
+        exist:       list of existence, e.g. [0, 1, 1, 0]
+        smooth:      whether to smooth the probability or not
+        y_px_gap:    y pixel gap for sampling
+        pts:     how many points for one lane
+        thresh:  probability threshold
+
+        Return:
+        ----------
+        coordinates: [x, y] list of lanes, e.g.: [ [[9, 569], [50, 549]] ,[[630, 569], [647, 549]] ]
+        """
+        if resize_shape is None:
+            resize_shape = seg_pred.shape[1:]  # seg_pred (5, h, w)
+        _, h, w = seg_pred.shape
+        H, W = resize_shape
+        coordinates = []
+
+        for i in range(7 - 1):
+            prob_map = seg_pred[i + 1]
+            if smooth:
+                prob_map = cv2.blur(prob_map, (9, 9), borderType=cv2.BORDER_REPLICATE)
+            coords = self.get_lane(prob_map, y_px_gap, pts, thresh, resize_shape)
+            if self.is_short(coords):
+                continue
+            coordinates.append(
+                [[coords[j], H - 10 - j * y_px_gap] if coords[j] > 0 else [-1, H - 10 - j * y_px_gap] for j in
+                 range(pts)])
+
+        if len(coordinates) == 0:
+            coords = np.zeros(pts)
+            coordinates.append(
+                [[coords[j], H - 10 - j * y_px_gap] if coords[j] > 0 else [-1, H - 10 - j * y_px_gap] for j in
+                 range(pts)])
+        # print(coordinates)
+
+        return coordinates
+
